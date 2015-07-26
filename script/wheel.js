2e31583ce112d8dd32a3953ef7163fb12e56955a
Brant-Ma <brant.mazhiyuan@gmail.com>
2015-07-26 14:10:45 +0800

    add some useful wheel

--- script/wheel.js
+++ script/wheel.js
@@ -20,3 +20,47 @@ function insertAfter(newElement,targetElement){
 		parent.insertBefore(newElement,targetElement.nextSibling);
 	}
 }
+
+// 轮子：追加元素的类
+function addClass(element,value){
+	if(!element.className){
+		element.className=value;
+	}else{
+		newClassName=element.className;
+		newClassName+=" ";
+		newClassName+=value;
+		element.className=newClassName;
+	}
+}
+
+// 轮子：定时移动元素
+function moveElement(elementID, x_final, y_final, interval){
+	var element=document.getElementById(elementID);
+	if(element.movement) clearTimeout(element.movement);
+	var distance=0;
+	if(!element.style.left) element.style.left="0px";
+	if(!element.style.top) element.style.top="0px";
+	var x=parseInt(element.style.left);
+	var y=parseInt(element.style.top);
+	if(x==x_final&&y==y_final) return true;
+	if(x<x_final){
+		distance=Math.ceil((x_final-x)/10);
+		x=x+distance;
+	} 
+	if(x>x_final){
+		distance=Math.ceil((x-x_final)/10);
+		x=x-distance;
+	} 
+	if(y<y_final){
+		distance=Math.ceil((y_final-y)/10);
+		y=y+distance;
+	} 
+	if(y>y_final){
+		distance=Math.ceil((y-y_final)/10);
+		y=y-distance;
+	} 
+	element.style.left=x+"px";
+	element.style.top=y+"px";
+	var temp="moveElement"+"('"+elementID+"',"+x_final+","+y_final+","+interval+")";
+	element.movement=setTimeout(temp,interval);
+}
