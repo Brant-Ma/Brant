1a5ef3b9b23f0f28bed5eb13690837469eadc4ee
Brant-Ma <brant.mazhiyuan@gmail.com>
2015-11-14 23:33:18 +0800

    try gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,37 @@
-var gulp = require("gulp");
+// 获取 gulp 和 modules
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	minifycss = require('gulp-minify-css'),
+	imagemin = require('gulp-imagemin'),
 
-gulp.task("default", function(){
+// js
+gulp.task('script', function() {
+	gulp.src('script/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/script'))
+})
 
-});
+// css
+gulp.task('style', function() {
+	gulp.src('style/*.css')
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist/style'))
+})
+
+// img
+gulp.task('image', function() {
+	gulp.src('picture/**/*.*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/image'))
+})
+
+// 默认任务
+gulp.task('default', ['script', 'style', 'image', 'watch'])
+
+
+// 监听任务
+gulp.task('watch', function() {
+	gulp.watch('script/*.js', ['script']);
+	gulp.watch('style/*.css', ['style']);
+	gulp.watch('picture/**/*.*', ['image']);
+})
