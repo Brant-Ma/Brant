eb3d36dbc9bf107dca9bf6f9dd173ca33c9844e6
Brant-Ma <brant.mazhiyuan@gmail.com>
2015-11-15 19:50:49 +0800

    compress the css and js code

--- script/changeTab.js
+++ script/changeTab.js
@@ -1,14 +1,1 @@
-//archive页面现在用css实现，changeTab现在退役了！
-
-//下面一行会造成：只有第一次激活态会正常显示原title，后来的激活态title会变为undefined
-//var content=document.getElementsByTagName("title").nodeValue;
-
-var content=document.title
-function changeTab(){
-	if(!document.hidden){
-		document.title=content;
-		}else{
-			document.title="(●––●) Hi, I'm Brant!";
-		}
-	}
-document.addEventListener("visibilitychange", changeTab);
+function changeTab(){document.hidden?document.title="(●––●) Hi, I'm Brant!":document.title=content}var content=document.title;document.addEventListener("visibilitychange",changeTab);
